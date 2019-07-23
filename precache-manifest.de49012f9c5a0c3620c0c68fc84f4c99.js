43c26cd64af54ecca77be1ec8a67e0021d081a8d
RZFalcore <box.rz.box@gmail.com>
2019-07-23 19:41:01 +0300

    Updates

---
+++ precache-manifest.de49012f9c5a0c3620c0c68fc84f4c99.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be2cd25c212a2121291b7b5ddc8b18d5",
+    "url": "/RZFalcore/goit-react-hw-02-movies/index.html"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/RZFalcore/goit-react-hw-02-movies/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fab7dd55c95b216188f2",
+    "url": "/RZFalcore/goit-react-hw-02-movies/static/js/main.7c3ee862.chunk.js"
+  },
+  {
+    "revision": "fe3c6faa8660869a8492",
+    "url": "/RZFalcore/goit-react-hw-02-movies/static/js/runtime~main.2b65f643.js"
+  }
+]);
